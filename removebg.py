c62f26e73122af6412d65b3c3b2536b93931647f
DinhChuong2909 <vudinhchuong2909@gmail.com>
2023-07-17 20:28:56 +0700

    Final

--- removebg.py
+++ removebg.py
@@ -1,7 +1,7 @@
 from rembg import remove
 from PIL import Image
 
-input_path = 'D:/mel.jpg'
+input_path = 'ONC.png'
 output_path = 'outputpic.png'
 input = Image.open(input_path)
 output = remove(input)
